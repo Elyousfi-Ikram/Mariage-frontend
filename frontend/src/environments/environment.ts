ca90095fc5a4c44bfb98c67d07c77f4bab83617b
EL YOUSFI Ikram <elyousfi.ikram@hotmail.com>
2026-01-24 21:51:48 +0100

    optimisation projet

--- frontend/src/environments/environment.ts
+++ frontend/src/environments/environment.ts
@@ -1,4 +1,5 @@
 export const environment = {
   production: false,
-  apiUrl: '/api'
+  apiUrl: '/api',
+  dbUrl: 'mongodb://localhost:27017/weddingmongodb+srv://ouistikram13_db_user:c5kBuafFBxAH6a1I@mariage.ivjpsop.mongodb.net/?appName=mariage'
 };
