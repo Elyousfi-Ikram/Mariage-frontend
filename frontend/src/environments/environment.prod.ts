37d8c2d7bc2b8027cea48ae2a4ca5d12b7ebde99
EL YOUSFI Ikram <elyousfi.ikram@hotmail.com>
2026-01-24 23:27:31 +0100

    optimisation des fichiers

--- frontend/src/environments/environment.prod.ts
+++ frontend/src/environments/environment.prod.ts
@@ -1,5 +1,4 @@
 export const environment = {
   production: true,
-  apiUrl: '/api', // You might want to change this to your actual production API URL
-  dbUrl: 'mongodb+srv://ouistikram13_db_user:c5kBuafFBxAH6a1I@mariage.ivjpsop.mongodb.net/?appName=mariage'
+  apiUrl: 'https://mariage-backend-y8bn.onrender.com/api'
 };
--- frontend/src/environments/environment.ts
+++ frontend/src/environments/environment.ts
@@ -1,5 +1,4 @@
 export const environment = {
   production: false,
-  apiUrl: '/api',
-  dbUrl: 'mongodb://localhost:27017/weddingmongodb+srv://ouistikram13_db_user:c5kBuafFBxAH6a1I@mariage.ivjpsop.mongodb.net/?appName=mariage'
+  apiUrl: 'http://localhost:3000/api'
 };
